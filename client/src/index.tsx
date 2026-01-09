f9f99c7d8d8a21c2265624cd00af7c3913b6e3a4
Christian Gawron <gawron.christian@fh-swf.de>
2026-01-09 11:41:16 +0000

    feat(client): sentry integration
    add sentry for client telemetry & feedback

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -25,6 +25,29 @@ import "./index.css";
 
 import { CONFIG } from "./helpers/config";
 
+import * as Sentry from "@sentry/react";
+
+if (CONFIG.SENTRY_DSN) {
+  Sentry.init({
+    dsn: CONFIG.SENTRY_DSN,
+    integrations: [
+      Sentry.browserTracingIntegration(),
+      Sentry.replayIntegration(),
+      Sentry.feedbackIntegration({
+        colorScheme: "system",
+        autoInject: false,
+      }),
+    ],
+    // Tracing
+    tracesSampleRate: 1.0, //  Capture 100% of the transactions
+    // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+    tracePropagationTargets: ["localhost", "appointme.gawron.cloud", /^\//],
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  });
+}
+
 const CLIENT_ID = CONFIG.CLIENT_ID;
 const BASE_PATH = CONFIG.BASE_PATH;
 
