926a7a92d714597028b35a2b41a8964480d27c4d
Christian Gawron <gawron.christian@fh-swf.de>
2026-01-28 12:20:17 +0100

    chore: Remove unused useState and useEffect imports

--- client/src/pages/App.tsx
+++ client/src/pages/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useContext, useEffect } from "react";
+import { useContext } from "react";
 import { Link as RouterLink } from "react-router-dom";
 import EventList from "../components/EventList";
 import AppNavbar from "../components/AppNavbar";
